af4b6974ea51a10d27e1865bcee7544f04ffcca5
Philip <p.gazagnes@vacanceselect.com>
2023-08-18 10:35:23 +0200

    simple page

--- app.vue
+++
@@ -1,8 +0,0 @@
-<script lang="ts" setup>
-</script>
-
-<template>
-  <div class="flex-1">
-    <NuxtWelcome />
-  </div>
-</template>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-slate-100 flex flex-col items-center justify-center h-screen">
+    <h1 class="font-semibold text-4xl mb-2">
+      L'Instant Live
+    </h1>
+    <p class="font-semibold mt-2 pb-3 px-2 border-b border-slate-200">
+      Production - Concerts - Sonorisation - Eclairage
+    </p>
+    <p class="mt-3">
+      10 rue de l'Aramon, 34540 Balaruc-le-Vieux
+    </p>
+    <p class="">
+      06.73.51.74.46
+    </p>
+  </div>
+</template>
